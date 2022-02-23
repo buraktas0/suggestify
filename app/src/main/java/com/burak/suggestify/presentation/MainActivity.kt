db180f147db649a72b55a9b8b11c6793bd813213
Burak <35180203+buraktas0@users.noreply.github.com>
2022-02-23 11:20:05 +0300

    viewBinding is implemented for main layout

--- app/src/main/java/com/burak/suggestify/presentation/MainActivity.kt
+++ app/src/main/java/com/burak/suggestify/presentation/MainActivity.kt
@@ -3,10 +3,13 @@ package com.burak.suggestify.presentation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.burak.suggestify.R
+import com.burak.suggestify.databinding.ActivityMainBinding
 import com.burak.suggestify.domain.model.request.ArtistRequest
+import com.burak.suggestify.domain.model.request.TrackRequest
 import com.burak.suggestify.presentation.ui.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.CoroutineScope
@@ -19,20 +22,71 @@ import kotlinx.coroutines.launch
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var viewModel : MainViewModel
+    private lateinit var viewModel: MainViewModel
+
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        bind()
+        observe()
+    }
+
+    private fun bind() {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
 
-        observe()
-
-        viewModel.getSimilarArtists(request = ArtistRequest(artist = "Gojira", null, null, null))
+        binding.searchButton.setOnClickListener {
+            search()
+        }
     }
 
     private fun observe() {
+        viewModel.similarArtistsLiveData.observe(this, Observer {
 
+        })
+
+        viewModel.similarTracksLiveData.observe(this, Observer {
+            
+        })
+    }
+
+    private fun search() {
+        binding.inputArtist.text?.toString()?.let { artist: String ->
+            {
+                binding.inputTrack.text?.toString()?.let { track: String ->
+                    {
+                        searchTrack(artist, track)
+                    }
+                } ?: searchArtist(artist)
+            }
+        }
+    }
+
+    private fun searchTrack(artist: String, track: String) {
+        viewModel.getSimilarTracks(
+            request = TrackRequest(
+                artist = artist,
+                track = track,
+                autoCorrect = 1,
+                limit = null,
+                mbid = null
+            )
+        )
+    }
+
+    private fun searchArtist(artist: String) {
+        viewModel.getSimilarArtists(
+            request = ArtistRequest(
+                artist = artist,
+                autoCorrect = 1,
+                limit = null,
+                mbid = null
+            )
+        )
     }
 }
--- app/src/main/java/com/burak/suggestify/presentation/ui/MainViewModel.kt
+++ app/src/main/java/com/burak/suggestify/presentation/ui/MainViewModel.kt
@@ -2,8 +2,11 @@ package com.burak.suggestify.presentation.ui
 
 import androidx.lifecycle.*
 import com.burak.suggestify.domain.model.request.ArtistRequest
+import com.burak.suggestify.domain.model.request.TrackRequest
 import com.burak.suggestify.domain.model.response.similarArtists.SimilarArtistsResponse
+import com.burak.suggestify.domain.model.response.similarTracks.SimilarTracksResponse
 import com.burak.suggestify.domain.usecase.artist.GetSimilarArtistUseCase
+import com.burak.suggestify.domain.usecase.track.GetSimilarTrackUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
@@ -13,18 +16,33 @@ import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
-    private val getSimilarArtistUseCase : GetSimilarArtistUseCase
+    private val getSimilarArtistUseCase: GetSimilarArtistUseCase,
+    private val getSimilarTrackUseCase: GetSimilarTrackUseCase
 ) : ViewModel() {
 
-    fun getSimilarArtists(request : ArtistRequest) {
+    val similarArtistsLiveData = MutableLiveData<SimilarArtistsResponse>()
+    val similarTracksLiveData = MutableLiveData<SimilarTracksResponse>()
+
+    fun getSimilarArtists(request: ArtistRequest) {
         viewModelScope.launch {
             getSimilarArtistUseCase.execute(request)
                 .catch {
 
                 }
                 .collect {
-                    val d = ""
+                    similarArtistsLiveData.postValue(it)
+                }
+        }
+    }
 
+    fun getSimilarTracks(request: TrackRequest) {
+        viewModelScope.launch {
+            getSimilarTrackUseCase.execute(request)
+                .catch {
+
+                }
+                .collect {
+                    similarTracksLiveData.postValue(it)
                 }
         }
     }
